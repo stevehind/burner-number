49dae892c2d0db780353b57d048f06cd225d8bf2
stevehind <steve.hind@gmail.com>
2020-06-18 09:53:39 -0700

    Refactored buying SMSNumber into the twilio_functions module.

--- vendor/twilio_functions.js
+++ vendor/twilio_functions.js
@@ -1,5 +1,6 @@
+// @flow
+
 // Credentials
-// $FlowFixMe
 const keys = require("../config/keys");
 // $FlowFixMe
 const client = require("twilio")(keys.twilioAccountSid, keys.twilioAuthToken);
@@ -16,7 +17,21 @@ type deleteAccountResultObject = {
     error: null | string
 }
 
-const createTwilioSubAccount = (friendlyName) => {
+type numberSearchPayload = {
+    smsEnabled: boolean
+}
+
+type buyNumberPayload = {
+    email: string,
+    sid: string
+}
+
+type buyNumberAttemptResult = {
+    succeeded: boolean,
+    error: null | string
+}
+
+const createTwilioSubAccount = (friendlyName: string) => {
     return client.api.accounts.create({
         friendlyName: friendlyName
     })
@@ -51,9 +66,41 @@ const deleteTwilioSubAccount = (sid: string): Promise<deleteAccountResultObject>
     });
 }
 
+const buyTwilioSMSNumber = (search_payload: numberSearchPayload, buy_payload: buyNumberPayload): Promise<buyNumberAttemptResult> => {
+    return client.availablePhoneNumbers('US').local.list(search_payload)
+    .then(list => {
+        return client.incomingPhoneNumbers.create({
+            phoneNumber: list[0].phoneNumber,
+            // Change this when in production.
+            smsUrl: "https://fbe30584.ngrok.io/api/numbers/receive"
+        })
+    })
+    .then(incoming_phone_number => {
+        return client.incomingPhoneNumbers(incoming_phone_number.sid)
+        .update({
+            accountSid: buy_payload.sid,
+            friendlyName: buy_payload.email
+        })
+    })
+    //TODO: make this return the actual result and handle it better. Needs to return something that can be saved to the database as an SMSNumber
+    .then(() => {
+        return {
+            succeeded: true,
+            error: null
+        };
+    })
+    .catch(error => {
+        return {
+            succeeded: false,
+            error: error
+        };
+    })
+}
+
 const twilio_functions = {
     createTwilioSubAccount: createTwilioSubAccount,
-    deleteTwilioSubAccount: deleteTwilioSubAccount
+    deleteTwilioSubAccount: deleteTwilioSubAccount,
+    buyTwilioSMSNumber: buyTwilioSMSNumber
 }
 
 module.exports = twilio_functions;
